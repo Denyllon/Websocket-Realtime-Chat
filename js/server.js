3f97cd34c508a9071c156a1c2437920270d5ff8f
Denyllon <daniel.studencki@gmail.com>
2017-05-23 00:16:28 +0200

    change index path in server.js

--- js/server.js
+++ js/server.js
@@ -4,7 +4,7 @@ const SocketServer = require('ws').Server;
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const INDEX = path.join(__dirname, '/../index.html');
 
 
 const server = express()
